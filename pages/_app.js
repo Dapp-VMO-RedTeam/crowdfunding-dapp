9d72be058c906baefd0c849fc01e2f0c86b2fefb
demo <phanthanh.trang200@gmail.com>
2021-12-09 07:58:56 +0700

    update pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,38 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { UseWalletProvider } from "use-wallet";
+import NavBar from "../components/Navbar";
+import Footer from "../components/Footer";
+import "@fontsource/space-grotesk";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Space Grotesk",
+    body: "Space Grotesk",
+  },
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <ChakraProvider theme={theme}>
+        <UseWalletProvider
+          chainId={4}
+          connectors={{
+            walletconnect: {
+              rpcUrl:
+                "https://rinkeby.infura.io/v3/83b57356a6be4e3e99890c21874b8d72",
+            },
+          }}
+        >
+          <NavBar />
+          <Component {...pageProps} />
+          <Footer />{" "}
+        </UseWalletProvider>
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,69 +1,347 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import { useEffect, useState } from "react";
+import NextLink from "next/link";
+import styles from "../styles/Home.module.css";
+import { getETHPrice, getWEIPriceInUSD } from "../lib/getETHPrice";
+import {
+  Heading,
+  useBreakpointValue,
+  useColorModeValue,
+  Text,
+  Button,
+  Flex,
+  Container,
+  SimpleGrid,
+  Box,
+  Divider,
+  Skeleton,
+  Img,
+  Icon,
+  chakra,
+  Tooltip,
+  Link,
+  SkeletonCircle,
+  HStack,
+  Stack,
+  Progress,
+} from "@chakra-ui/react";
 
-export default function Home() {
+import factory from "../contracts/factory";
+import web3 from "../smart-contract/web3";
+import Campaign from "../contracts/Campaign";
+import { ExternalLinkIcon } from "@chakra-ui/icons";
+import { FaHandshake } from "react-icons/fa";
+import { FcShare, FcDonate, FcMoneyTransfer } from "react-icons/fc";
+
+export async function getServerSideProps(context) {
+  const campaigns = await factory.methods.getDeployedCampaigns().call();
+
+  console.log(campaigns);
+
+  return {
+    props: { campaigns },
+  };
+}
+
+const Feature = ({ title, text, icon }) => {
   return (
-    <div className={styles.container}>
+    <Stack>
+      <Flex
+        w={16}
+        h={16}
+        align={"center"}
+        justify={"center"}
+        color={"white"}
+        rounded={"full"}
+        bg={useColorModeValue("gray.100", "gray.700")}
+        mb={1}
+      >
+        {icon}
+      </Flex>
+      <Text fontWeight={600}>{title}</Text>
+      <Text color={useColorModeValue("gray.500", "gray.200")}>{text}</Text>
+    </Stack>
+  );
+};
+
+function CampaignCard({
+  name,
+  description,
+  creatorId,
+  imageURL,
+  id,
+  balance,
+  target,
+  ethPrice,
+}) {
+  return (
+    <NextLink href={`/campaign/${id}`}>
+      <Box
+        bg={useColorModeValue("white", "gray.800")}
+        maxW={{ md: "sm" }}
+        borderWidth="1px"
+        rounded="lg"
+        shadow="lg"
+        position="relative"
+        alignItems="center"
+        justifyContent="center"
+        cursor="pointer"
+        transition={"transform 0.3s ease"}
+        _hover={{
+          transform: "translateY(-8px)",
+        }}
+      >
+        <Box height="18em">
+          <Img
+            src={imageURL}
+            alt={`Picture of ${name}`}
+            roundedTop="lg"
+            objectFit="cover"
+            w="full"
+            h="full"
+            display="block"
+          />
+        </Box>
+        <Box p="6">
+          <Flex
+            mt="1"
+            justifyContent="space-between"
+            alignContent="center"
+            py={2}
+          >
+            <Box
+              fontSize="2xl"
+              fontWeight="semibold"
+              as="h4"
+              lineHeight="tight"
+              isTruncated
+            >
+              {name}
+            </Box>
+
+            <Tooltip
+              label="Contribute"
+              bg={useColorModeValue("white", "gray.700")}
+              placement={"top"}
+              color={useColorModeValue("gray.800", "white")}
+              fontSize={"1.2em"}
+            >
+              <chakra.a display={"flex"}>
+                <Icon
+                  as={FaHandshake}
+                  h={7}
+                  w={7}
+                  alignSelf={"center"}
+                  color={"teal.400"}
+                />{" "}
+              </chakra.a>
+            </Tooltip>
+          </Flex>
+          <Flex alignContent="center" py={2}>
+            {" "}
+            <Text color={"gray.500"} pr={2}>
+              by
+            </Text>{" "}
+            <Heading size="base" isTruncated>
+              {creatorId}
+            </Heading>
+          </Flex>
+          <Flex direction="row" py={2}>
+            <Box w="full">
+              <Box
+                fontSize={"2xl"}
+                isTruncated
+                maxW={{ base: "	15rem", sm: "sm" }}
+                pt="2"
+              >
+                <Text as="span" fontWeight={"bold"}>
+                  {balance > 0
+                    ? web3.utils.fromWei(balance, "ether")
+                    : "0, Trở thành người hỗ trợ chiến dịch 😄"}
+                </Text>
+                <Text
+                  as="span"
+                  display={balance > 0 ? "inline" : "none"}
+                  pr={2}
+                  fontWeight={"bold"}
+                >
+                  {" "}
+                  ETH
+                </Text>
+                <Text
+                  as="span"
+                  fontSize="lg"
+                  display={balance > 0 ? "inline" : "none"}
+                  fontWeight={"normal"}
+                  color={useColorModeValue("gray.500", "gray.200")}
+                >
+                  (${getWEIPriceInUSD(ethPrice, balance)})
+                </Text>
+              </Box>
+
+              <Text fontSize={"md"} fontWeight="normal">
+                target of {web3.utils.fromWei(target, "ether")} ETH ($
+                {getWEIPriceInUSD(ethPrice, target)})
+              </Text>
+              <Progress
+                colorScheme="teal"
+                size="sm"
+                value={web3.utils.fromWei(balance, "ether")}
+                max={web3.utils.fromWei(target, "ether")}
+                mt="2"
+              />
+            </Box>{" "}
+          </Flex>
+        </Box>
+      </Box>
+    </NextLink>
+  );
+}
+
+export default function Home({ campaigns }) {
+  const [campaignList, setCampaignList] = useState([]);
+  const [ethPrice, updateEthPrice] = useState(null);
+
+  async function getSummary() {
+    try {
+      const summary = await Promise.all(
+        campaigns.map((campaign, i) =>
+          Campaign(campaigns[i]).methods.getSummary().call()
+        )
+      );
+      const ETHPrice = await getETHPrice();
+      updateEthPrice(ETHPrice);
+      console.log("summary ", summary);
+      setCampaignList(summary);
+
+      return summary;
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getSummary();
+  }, []);
+
+  return (
+    <div>
       <Head>
-        <title>Create Next App</title>
-        <meta name="description" content="Generated by create next app" />
-        <link rel="icon" href="/favicon.ico" />
+        <title>Diều Xanh</title>
+        <meta
+          name="description"
+          content="Một ứng dụng kêu gọi vốn hỗ trợ cộng đồng dựa trên công nghệ Blockchain  "
+        />
+        <link rel="icon" href="/logo.svg" />
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Learn <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+        <Container py={{ base: "4", md: "12" }} maxW={"7xl"} align={"left"}>
+          {" "}
+          <Heading
+            textAlign={useBreakpointValue({ base: "left" })}
+            fontFamily={"heading"}
+            color={useColorModeValue("gray.800", "white")}
+            as="h1"
+            py={4}
           >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+            <br /> Crypto & Blockchain 😄{" "}
+          </Heading>
+          <NextLink href="/campaign/new">
+            <Button
+              display={{ sm: "inline-flex" }}
+              fontSize={"md"}
+              fontWeight={600}
+              color={"white"}
+              bg={"teal.400"}
+              _hover={{
+                bg: "teal.300",
+              }}
+            >
+              Tạo chiến dịch mới 
+            </Button>
+          </NextLink>
+        </Container>
+        <Container py={{ base: "4", md: "12" }} maxW={"7xl"}>
+          <HStack spacing={2}>
+            <SkeletonCircle size="4" />
+            <Heading as="h2" size="lg">
+              Mở chiến dịch 
+            </Heading>
+          </HStack>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <Divider marginTop="4" />
+
+          {campaignList.length > 0 ? (
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
+              {campaignList.map((el, i) => {
+                return (
+                  <div key={i}>
+                    <CampaignCard
+                      name={el[5]}
+                      description={el[6]}
+                      creatorId={el[4]}
+                      imageURL={el[7]}
+                      id={campaigns[i]}
+                      target={el[8]}
+                      balance={el[1]}
+                      ethPrice={ethPrice}
+                    />
+                  </div>
+                );
+              })}
+            </SimpleGrid>
+          ) : (
+            <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
+              <Skeleton height="25rem" />
+              <Skeleton height="25rem" />
+              <Skeleton height="25rem" />
+            </SimpleGrid>
+          )}
+        </Container>
+        <Container py={{ base: "4", md: "12" }} maxW={"7xl"} id="howitworks">
+          <HStack spacing={2}>
+            <SkeletonCircle size="4" />
+            <Heading as="h2" size="lg">
+              Hoạt động 
+            </Heading>
+          </HStack>
+          <Divider marginTop="4" />
+          <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10} py={8}>
+            <Feature
+              icon={<Icon as={FcDonate} w={10} h={10} />}
+              title={"Tạo chiến dịch gây quỹ "}
+              text={
+                "Điền thông tin cho chiến dịch của bạn nào!"
+              }
+            />
+            <Feature
+              icon={<Icon as={FcShare} w={10} h={10} />}
+              title={"Chia sẻ chiến dịch "}
+              text={
+                "Hãy chia sẻ chiến dịch của bạn "
+              }
+            />
+            <Feature
+              icon={<Icon as={FcMoneyTransfer} w={10} h={10} />}
+              title={"Request and Withdraw Funds"}
+              text={
+                "The funds raised can be withdrawn directly to the recipient when 50% of the contributors approve of the Withdrawal Request."
+              }
+            />
+          </SimpleGrid>
+          <Heading as="h2" size="lg" mt="8">
+            Hãy đưa các câu hỏi qua {" "}
+            <Link
+              color="teal.500"
+              href="https://github.com/harsh242/betterfund-crowdfunding-in-blockchain/issues"
+              isExternal
+            >
+               Github Repo <ExternalLinkIcon mx="2px" />
+            </Link>{" "}
+          </Heading>
+          <Divider marginTop="4" />
+        </Container>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
     </div>
-  )
+  );
 }
